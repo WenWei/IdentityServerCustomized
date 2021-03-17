1c9f2ff228cae559ba87cdd9bd334f20846e201f
Alger <alger.chen23@gmail.com>
2021-03-17 12:03:21 +0800

    Add signing credential for production

--- IdentityServerCustomized/Startup.cs
+++ IdentityServerCustomized/Startup.cs
@@ -12,29 +12,25 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using static IdentityServer4.IdentityServerConstants;
 
 namespace IdentityServerCustomized
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IWebHostEnvironment webEnvironment)
         {
             Configuration = configuration;
+            WebEnvironment = webEnvironment;
         }
 
         public IConfiguration Configuration { get; }
+        public IWebHostEnvironment WebEnvironment { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddIdentityServer()
-                    .AddDeveloperSigningCredential()
-                    .AddConfigurationStore(option =>
-                           option.ConfigureDbContext = builder => builder.UseNpgsql(Configuration.GetConnectionString("IdentityServerConnection"), options =>
-                           options.MigrationsAssembly("IdentityServerCustomized.Postgresql")))
-                    .AddOperationalStore(option =>
-                           option.ConfigureDbContext = builder => builder.UseNpgsql(Configuration.GetConnectionString("IdentityServerConnection"), options =>
-                           options.MigrationsAssembly("IdentityServerCustomized.Postgresql")));
+            IdentityServerConfigure(services, WebEnvironment);
 
             services.AddControllers();
         }
@@ -60,6 +56,26 @@ namespace IdentityServerCustomized
             {
                 endpoints.MapControllers();
             });
+        }
+
+        private void IdentityServerConfigure(IServiceCollection services, IWebHostEnvironment env)
+        {
+            var builder = services.AddIdentityServer();
+
+#if DEBUG
+            builder.AddDeveloperSigningCredential();
+#else
+            var rsa = new RsaKeyService(env, TimeSpan.FromDays(30));
+            services.AddSingleton<RsaKeyService>(provider => rsa);
+            builder.AddSigningCredential(rsa.GetKey(), RsaSigningAlgorithm.RS256);
+#endif
+
+            builder.AddConfigurationStore(option =>
+                          option.ConfigureDbContext = builder => builder.UseNpgsql(Configuration.GetConnectionString("IdentityServerConnection"), options =>
+                          options.MigrationsAssembly("IdentityServerCustomized.Postgresql")))
+                   .AddOperationalStore(option =>
+                          option.ConfigureDbContext = builder => builder.UseNpgsql(Configuration.GetConnectionString("IdentityServerConnection"), options =>
+                          options.MigrationsAssembly("IdentityServerCustomized.Postgresql")));
         }
     }
 }
